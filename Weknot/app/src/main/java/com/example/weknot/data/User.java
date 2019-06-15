81ee315a3bbfcec04e58008e78b63c8ee3c83b7c
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-06-15 11:19:54 +0900

    user

--- Weknot/app/src/main/java/com/example/weknot/data/User.java
+++ Weknot/app/src/main/java/com/example/weknot/data/User.java
@@ -1,5 +1,7 @@
 package com.example.weknot.data;
 
+import java.util.Date;
+
 public class User {
 
     String id;
@@ -8,7 +10,97 @@ public class User {
 
     String password;
 
-    String birth;
+    Date birth;
 
     String gender;
+
+    String phoneNumber;
+
+    String picture;
+
+    String intro;
+
+    String scope;
+
+    int point;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
 }
