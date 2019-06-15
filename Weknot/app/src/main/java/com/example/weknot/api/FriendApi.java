339e8cedc7f17b9820b06df1c8ab6caccd4958a5
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-06-15 14:49:40 +0900

    friend

--- Weknot/app/src/main/java/com/example/weknot/api/FriendApi.java
+++ Weknot/app/src/main/java/com/example/weknot/api/FriendApi.java
@@ -1,4 +1,31 @@
 package com.example.weknot.api;
 
+import com.example.weknot.data.Friend;
+import com.example.weknot.data.Result;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface FriendApi {
+
+    @FormUrlEncoded
+    @POST("")
+    Call<List<Friend>> getFriends(@Field("id") String id);
+
+    @FormUrlEncoded
+    @POST("")
+    Call<List<Friend>> getRequesters(@Field("id") String id);
+
+    @FormUrlEncoded
+    @POST("")
+    Call<Result> addFriend(@Field("userId") String userId, @Field("friendId") String friendId);
+
+    @FormUrlEncoded
+    @POST("")
+    Call<Result> checkFriend(@Field("userId") String userId, @Field("friendId") String friendId);
+
 }
