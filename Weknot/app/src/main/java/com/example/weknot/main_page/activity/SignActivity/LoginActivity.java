68b75f466f806597bcca4b941442b9e538426391
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-06-29 15:56:24 +0900

    .

--- Weknot/app/src/main/java/com/example/weknot/main_page/activity/SignActivity/LoginActivity.java
+++ Weknot/app/src/main/java/com/example/weknot/main_page/activity/SignActivity/LoginActivity.java
@@ -74,7 +74,6 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         }
-
     }
 
     private void clickEvent() {
