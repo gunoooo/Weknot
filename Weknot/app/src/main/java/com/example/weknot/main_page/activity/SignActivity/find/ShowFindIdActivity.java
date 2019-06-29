5e8b6b79a59f454925b8ce9459b529437ed9d043
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-06-29 10:28:17 +0900

    findId

---
+++ Weknot/app/src/main/java/com/example/weknot/main_page/activity/SignActivity/find/ShowFindIdActivity.java
@@ -0,0 +1,16 @@
+package com.example.weknot.main_page.activity.SignActivity.find;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.weknot.R;
+
+public class ShowFindIdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_find_id);
+    }
+}
