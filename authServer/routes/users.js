14c255dcfe29d0b31c11243e6b5948c7fb46b1b4
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-09-08 15:25:14 +0900

    [fix] users auth users
    function을 arrow function으로 변경하고
    addFriend 받는 변수 이름을 바꿈,  users query 제대로 작동하지 않아 배열로 넣어줌

--- authServer/routes/users.js
+++ authServer/routes/users.js
@@ -3,44 +3,41 @@ const router = express.Router();
 const mariadb = require('mariadb');
 const dbcp = require('../models/dbcp');
 const users = require('../models/users');
+const authMiddle = require("../middlewares/auth");
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   const pool = mariadb.createPool(dbcp);
   users.allUsers(pool)
-    .then(function(result){
+    .then((result) => {
       console.log(result);
       res.send(JSON.stringify(result));
     })
-    .catch(function(err){
+    .catch((err) => {
       res.render('error');
     });
 });
 
-router.get('/login',function(req,res,next){
-  res.render('loginForm');
-});
-
-router.post('/login',function(req,res,next){
+router.post('/login', (req,res,next) => {
   let userId = req.body.id;
   let password = req.body.password;
 
   users.queryUser(userId)
-  .then(function(result){
+  .then((result) => {
     if(result.password === password){
       res.json({result:"success"});
     }
     else
       res.json({result:"fail"});
   })
-  .catch(function(err){
+  .catch((err) => {
     res.render('error', {error:err});
   });
 });
 
 
-router.post('/register',function(req,res,next) {
+router.post('/register',(req,res,next) => {
   let user = {
     id : req.body.id,
     name : req.body.name,
@@ -51,22 +48,35 @@ router.post('/register',function(req,res,next) {
   };
 
   users.registerUser(user)
-  .then(function(result){
-    res.send('{"result":"sucess"}')
+  .then((result)=>{
+    if(result.affectedRows === 1){
+      res.json({result:"success"});
+    }else{
+      res.status(403).json({
+        message:'omg'
+      })
+    }
+    
   })
-  .catch(function(err){
-    res.render('error');
+  .catch((err) => {
+    res.status(403).json({
+      message:err.message,
+    });
   })
 });
 
-router.post('/changePassword', (req,res,next) => {
-  const userId = "wowjddl";
-  const newPassword = "abcd";
+router.post('/changePassword',authMiddle, (req,res,next) => {
+  const userId = req.decodedToken.sub;
+  const newPassword = req.body.newPassword;
 
-  res.json({
-    result:"success",
-    message: "ok"
-  });
+  users.changePassword(userId, newPassword)
+  .then((result) => {
+    res.json({
+      result:"success",
+      message: "ok"
+    });
+  })
+  
 });
 
 router.post('/like', (req, res, next) => {
