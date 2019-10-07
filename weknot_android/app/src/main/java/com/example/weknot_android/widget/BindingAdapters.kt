b7e16384d5b3d5428a929fdd044ab34313db65e6
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-10-07 09:45:49 +0900

    [FIX] BindingAdapter
    what?
    Glide 예외처리

--- weknot_android/app/src/main/java/com/example/weknot_android/widget/BindingAdapters.kt
+++ weknot_android/app/src/main/java/com/example/weknot_android/widget/BindingAdapters.kt
@@ -10,6 +10,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.request.RequestOptions
+import com.example.weknot_android.R
 import net.gahfy.mvvmposts.utils.extension.getParentActivity
 
 @BindingAdapter("adapter")
@@ -43,16 +46,36 @@ fun setMutableImageDrawable(view: ImageView, resid: MutableLiveData<Int>?) {
 
 @BindingAdapter("mutableImageUrl")
 fun setMutableImageUrl(view: ImageView, url: MutableLiveData<String>?) {
+    val requestOptions: RequestOptions by lazy {
+        RequestOptions()
+                .error(R.drawable.profile)
+                .placeholder(R.drawable.loading)
+                .transforms(CenterCrop())
+    }
+
     val parentActivity:AppCompatActivity? = view.getParentActivity()
     if(parentActivity != null && url != null) {
-        url.observe(parentActivity, Observer { value -> Glide.with(view.context).load(value).into(view)})
+        url.observe(parentActivity, Observer { value -> Glide.with(view.context)
+                .load(value)
+                .apply(requestOptions)
+                .into(view)})
     }
 }
 
 @BindingAdapter("mutableImageUri")
 fun setMutableImageUri(view: ImageView, uri: MutableLiveData<Uri>?) {
+    val requestOptions: RequestOptions by lazy {
+        RequestOptions()
+                .error(R.drawable.profile)
+                .placeholder(R.drawable.loading)
+                .transforms(CenterCrop())
+    }
+
     val parentActivity:AppCompatActivity? = view.getParentActivity()
     if(parentActivity != null && uri != null) {
-        uri.observe(parentActivity, Observer { value -> Glide.with(view.context).load(value).into(view)})
+        uri.observe(parentActivity, Observer { value -> Glide.with(view.context)
+                .load(value)
+                .apply(requestOptions)
+                .into(view)})
     }
 }
