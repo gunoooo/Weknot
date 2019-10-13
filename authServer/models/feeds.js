538209cedf0edf4fe318b91c38ac501149316e53
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-10-13 10:56:03 +0900

    [FIx] fedds
    일단 라이크 카운트 얻을 수 있게 만듦.

--- authServer/models/feeds.js
+++ authServer/models/feeds.js
@@ -52,15 +52,18 @@ exports.getFeeds = async (id) => {
   const sql = '(SELECT count(`like`.id) as likeCount, feed.id as feedId, feed.writer, feed.time, feed.picture, feed.comment, user.photo, user.name, (SELECT COUNT(id) FROM `like` WHERE `like`.feedId = feed.id) AS feedCount FROM feed ' +
     'JOIN user ON feed.writer = user.id ' +
     'JOIN friends ON feed.writer = friends.receiver ' +
+    'JOIN `like` ON feedId = `like`.feedId' +
     'WHERE friends.state = 1 AND friends.requester = ?) ' +
     'UNION ' +
     '(SELECT count(`like`.id) as likeCount, feed.id as feedId, feed.writer, feed.time, feed.picture, feed.comment, user.photo, user.name,(SELECT COUNT(id) FROM `like` WHERE `like`.feedId = feed.id) AS feedCount FROM feed ' +
     'JOIN user ON feed.writer = user.id ' +
     'JOIN friends ON feed.writer = friends.requester ' +
+    'JOIN `like` ON feedId = `like`.feedId' +
     'WHERE friends.state = 1 AND friends.receiver = ?) ' +
     'UNION ' +
     '(SELECT count(`like`.id) as likeCount, feed.id as feedId, feed.writer, feed.time, feed.picture, feed.comment, user.photo, user.name,(SELECT COUNT(id) FROM `like` WHERE `like`.feedId = feed.id) AS feedCount FROM feed ' +
     'JOIN user ON feed.writer = user.id ' +
+    'JOIN `like` ON feedId = `like`.feedId' +
     'WHERE feed.writer = ?) ' +
     'ORDER BY time DESC; ';
 
